21bd73faa1fae83b6b0912b195a40bc7b3802ed2
khoussngom <khoussn@gmail.com>
2025-04-19 20:27:32 +0000

    ok

--- app/models/model.php
+++ app/models/model.php
@@ -1,30 +1,14 @@
 <?php
 declare(strict_types=1);
-
 namespace App\Models;
 
 use Chemins;
 
 $databaseFile = __DIR__ . Chemins::DataJson->value;
 
-$chargerDatabase = function (string $file): array {
-    if (!file_exists($file)) {
-        return [];
-    }
-    $json = file_get_contents($file);
-    return json_decode($json, true) ?? [];
-};
-
-$saveDatabase = function (string $file, array $data): void {
-    $json = json_encode($data, JSON_PRETTY_PRINT);
-    file_put_contents($file, $json);
-};
-
-$database = $loadDatabase($databaseFile);
+$database = json_decode(file_get_contents($databaseFile), true);
 
 return [
     "database" => $database,
-    "databaseFile" => $databaseFile,
-    "loadDatabase" => $chargerDatabase,
-    "saveDatabase" => $saveDatabase,
+    "databaseFile" => $databaseFile  
 ];
